866c42c67ffe58087ad7e6d12359aa36bb9d3328
goreleaserbot <walz@reconbuddy.com>
2020-10-19 13:00:49 -0400

    Brew formula update for bit version v0.6.14

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -2,16 +2,16 @@
 class Bit < Formula
   desc "A modern git CLI"
   homepage "https://github.com/chriswalz/bit"
-  version "0.6.12"
+  version "0.6.14"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chriswalz/bit/releases/download/v0.6.12/bit_0.6.12_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "2bbf748957a4477866f973a85b1e8277e6aaed7e7b98f16cb4f78477f1bd7edc"
+    url "https://github.com/chriswalz/bit/releases/download/v0.6.14/bit_0.6.14_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy.
+    sha256 "2150d06d867482a62bebfb85c86252ed97d7ad8d8e5c313af578b06b8f2375fa"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chriswalz/bit/releases/download/v0.6.12/bit_0.6.12_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "21dc2f40d117568bd130c2a0dca9b29a2522d4093ec86fb126a549dca3d2d91a"
+      url "https://github.com/chriswalz/bit/releases/download/v0.6.14/bit_0.6.14_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy.
+      sha256 "6cef74fc855045bb2808bdf33b03902e10b81d38a6405aa1a4799c38bbc517e3"
     end
   end
 
